816a40927aca9f3ac4b80864187e2feee0bf162f
Lakhan Tekchandani <lakhantekchandani400@gmail.com>
2026-06-08 15:37:20 +0530

    Complete CRUD operations

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -12,9 +12,61 @@
                res.status(400).json({ error: error.message });
            }
         };
-
+        const getEmployees = async (req, res)=>{
+            try{
+                const employees = await Employee.find();
+                res.status(200).json({
+                    message:"Employees fetched succesfully",
+                    employees:employees
+                })
+            }
+            catch(error){
+                res.status(400).json({ error: error.message });
+            }
+        }
+         const getEmployee = async (req, res)=>{
+            try{
+                const employee = await Employee.findById(req.params.id);
+                res.status(200).json({
+                    message:"Employee fetched succesfully",
+                    employee:employee
+                })
+            }
+            catch(error){
+                res.status(400).json({ error: error.message });
+            }
+        }
+        const updateEmployee= async (req,res)=>{
+            try{
+                const empolyee = await Employee.findByIdAndUpdate(req.params.id,req.body,{new:true});
+                res.status(200).json({
+                    message:"Employee updated succesfully",
+                    employee:empolyee
+                })
+            }
+            catch(error){
+                res.status(400).json({ error: error.message });
+            }
+        }
+        const deleteEmployee= async (req,res)=>{
+            try{
+               const employee =await Employee.findByIdAndDelete(req.params.id);
+               res.status(200).json({
+                message:"Employee deleted succesfully",
+                employee:employee
+               })
+                
+            }
+            catch(error){
+                res.status(400).json({ error: error.message });
+            }
+        }
         
 
         module.exports = {
-            createEmployee
+            createEmployee,
+            getEmployees,
+            getEmployee,
+            updateEmployee,
+            deleteEmployee
         };
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { createEmployee } = require("../controllers/employeeController");
+const { getEmployee } = require("../controllers/employeeController");
+const { getEmployees } = require("../controllers/employeeController");
+const { updateEmployee } = require("../controllers/employeeController");
+const { deleteEmployee } = require("../controllers/employeeController");
 
 router.post("/employees", createEmployee);
-
-
+router.get("/employees",getEmployees);
+router.get("/employees/:id",getEmployee);
+router.patch("/employees/:id",updateEmployee);
+router.delete("/employees/:id",deleteEmployee);
 
 
 module.exports = router;
